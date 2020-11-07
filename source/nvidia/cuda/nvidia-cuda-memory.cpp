8fdb265a5b99f3816031474f6c16ba07a787115a
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2020-11-07 09:00:12 +0100

    nvidia/cuda/memory: Don't require passing CUDA library

--- source/nvidia/cuda/nvidia-cuda-memory.cpp
+++ source/nvidia/cuda/nvidia-cuda-memory.cpp
@@ -19,22 +19,40 @@
 
 #include "nvidia-cuda-memory.hpp"
 #include <stdexcept>
+#include "util/util-logging.hpp"
 
-nvidia::cuda::memory::memory(std::shared_ptr<::nvidia::cuda::cuda> cuda, std::size_t size)
-	: _cuda(cuda), _pointer(), _size(size)
+#ifdef _DEBUG
+#define ST_PREFIX "<%s> "
+#define D_LOG_ERROR(x, ...) P_LOG_ERROR(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_WARNING(x, ...) P_LOG_WARN(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_INFO(x, ...) P_LOG_INFO(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_DEBUG(x, ...) P_LOG_DEBUG(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#else
+#define ST_PREFIX "<nvidia::cuda::memory> "
+#define D_LOG_ERROR(...) P_LOG_ERROR(ST_PREFIX __VA_ARGS__)
+#define D_LOG_WARNING(...) P_LOG_WARN(ST_PREFIX __VA_ARGS__)
+#define D_LOG_INFO(...) P_LOG_INFO(ST_PREFIX __VA_ARGS__)
+#define D_LOG_DEBUG(...) P_LOG_DEBUG(ST_PREFIX __VA_ARGS__)
+#endif
+
+nvidia::cuda::memory::~memory()
 {
-	::nvidia::cuda::result res = _cuda->cuMemAlloc(&_pointer, size);
+	D_LOG_DEBUG("Finalizing... (Addr: 0x%" PRIuPTR ")", this);
+
+	_cuda->cuMemFree(_pointer);
+}
+
+nvidia::cuda::memory::memory(size_t size) : _cuda(::nvidia::cuda::cuda::get()), _pointer(), _size(size)
+{
+	D_LOG_DEBUG("Initializating... (Addr: 0x%" PRIuPTR ")", this);
+
+	::nvidia::cuda::result res = _cuda->cuMemAlloc(&_pointer, _size);
 	switch (res) {
 	case ::nvidia::cuda::result::SUCCESS:
 		break;
 	default:
 		throw std::runtime_error("nvidia::cuda::memory: cuMemAlloc failed.");
 	}
-}
-
-nvidia::cuda::memory::~memory()
-{
-	_cuda->cuMemFree(_pointer);
 }
 
 nvidia::cuda::device_ptr_t nvidia::cuda::memory::get()
